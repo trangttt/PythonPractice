6595da96f5cacb5403dc0de1d74a0d9a7e51b202
Trang Tran Thi Thuy <thuytranga1@yahoo.com>
2014-08-26 17:08:13 +0300

    RollerCoaster

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,1 +1,25 @@
+import sys
+
+def load_file(fn):
+    data = []
+    with open(fn) as f:
+        for line in f:
+            data.append(line)
+    return data
+
+def rollercoaster(data):
+    for line in data:
+        result = []
+        flag = 0
+        for l in line.strip():
+            if l  in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' :
+                l = l.upper() if flag == 0 else l
+                flag = 1 if flag  == 0 else 0
+            result.append(l)
+        print "".join(result)
+
+
+if __name__ == "__main__":
+    rollercoaster(load_file(sys.argv[1]))
+
 __author__ = 'dantoccatu'
