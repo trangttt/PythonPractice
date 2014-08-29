13a6e5b7fc608578947403a2e690d926dadb5c9e
Trang Tran Thi Thuy <thuytranga1@yahoo.com>
2014-08-29 13:03:36 +0300

    String Permutations

--- string_permutations.py
+++ string_permutations.py
@@ -1,1 +1,28 @@
-__author__ = 'dantoccatu'
+import sys
+import string
+
+
+def load_file(fn):
+    data = []
+    with open(fn) as f:
+        for line in f:
+            data.append(line.strip())
+    return data
+
+def string_permutation(word):
+    if len(word) == 1 : return [word]
+
+    result = []
+    l = list(word)
+    for i in sorted(l):
+        prev_permutation =  string_permutation(string.replace(word, i, ''))
+        for j in prev_permutation :
+            result.append(string.join([i, j], ""))
+    return result
+
+def string_permutations(data):
+    for line in data:
+        print ",".join(string_permutation(line))
+
+if __name__ == "__main__":
+    string_permutations(load_file(sys.argv[1]))
